8ca1464a8d67039ab07886282f86ec857f83b452
Gparracec <121050193+Gparracec@users.noreply.github.com>
2022-12-21 16:25:26 -0300

    Andanto, por fin

---
+++ reto21.js
@@ -0,0 +1,14 @@
+function printTable(gifts) {
+    return ''
+  }
+
+printTable([
+    { name: 'Game', quantity: 2 },
+    { name: 'Bike', quantity: 1 },
+    { name: 'Book', quantity: 3 },
+  ])
+
+  printTable([
+    { name: 'PlayStation 5', quantity: 9234782374892 },
+    { name: 'Book Learn Web Dev', quantity: 23531 }
+  ])
